95d2e41fefeceb17746723441af7d37f7a813cd0
emmanuellim450 <23041351@myrp.edu.sg>
2025-11-10 18:02:04 +0800

    Railway MySQL config with SSL fix

--- config/database.js
+++ config/database.js
@@ -3,14 +3,16 @@ const path = require('path');
 module.exports = ({ env }) => {
   return {
     connection: {
-      client: 'mysql', // use MySQL
+      client: 'mysql',
       connection: {
-        host: env('DATABASE_HOST', 'caboose.proxy.rlwy.net'),   // Railway public host
-        port: env.int('DATABASE_PORT', 50921),                  // Railway public port
-        database: env('DATABASE_NAME', 'railway'),             // Railway database name
-        user: env('DATABASE_USERNAME', 'root'),                // Railway username
+        host: env('DATABASE_HOST', 'caboose.proxy.rlwy.net'),  // Railway public host
+        port: env.int('DATABASE_PORT', 50921),                 // Railway public port
+        database: env('DATABASE_NAME', 'railway'),            // Railway database name
+        user: env('DATABASE_USERNAME', 'root'),               // Railway username
         password: env('DATABASE_PASSWORD', 'cWgXLDruvgvRGqBhbOvYfQjFOmUTwaAQ'), // Railway password
-        ssl: env.bool('DATABASE_SSL', true),                   // Railway requires SSL for public connections
+        ssl: {
+          rejectUnauthorized: false, // allows self-signed certificate
+        },
       },
       pool: { min: 2, max: 10 },
       acquireConnectionTimeout: 60000,
